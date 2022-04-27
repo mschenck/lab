cde71ea33c8a09f373e4dd61257fd963e4c2ea01
Michael Schenck <mschenck@digitalocean.com>
2022-04-27 16:22:14 -0400

    deployed a container registry, wrote up (but commented out - due to no current support) helm registry

--- stack/gcp/main.tf
+++ stack/gcp/main.tf
@@ -9,7 +9,30 @@ module "k8s-infra" {
   source = "./k8s-infra"
 
   project_id   = "stack-348418"
-  cluster_name = "gke-stack"
   region       = "us-central1"
   zones        = ["us-central1-a"]
+
+  cluster_name = "gke-stack"
+}
+
+#module "helm_registry" {
+#  source = "./artifact_registry"
+#
+#  project_id        = "stack-348418"
+#  region            = "us-central1"
+#  zones             = ["us-central1-a"]
+#
+#  repository_id     = "helm-registry"
+#  repository_format = "HELM"
+#}
+
+module "container_registry" {
+  source = "./artifact_registry"
+
+  project_id        = "stack-348418"
+  region            = "us-central1"
+  zones             = ["us-central1-a"]
+
+  repository_id     = "container-registry"
+  repository_format = "DOCKER"
 }
