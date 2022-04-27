237776410fd144e6df3c1d3d5fefa8bd9bdc3ebd
Michael Schenck <mschenck@digitalocean.com>
2022-04-27 16:21:33 -0400

    fixed artifact_registry variable mapping

--- stack/gcp/artifact_registry/artifact_registry.tf
+++ stack/gcp/artifact_registry/artifact_registry.tf
@@ -1,7 +1,7 @@
 resource "google_artifact_registry_repository" "artifact_registry" {
   provider = google-beta
 
-  location          = var.region
-  repository_id     = var.repository_id
-  repository_format = var.repository_format
+  location      = var.region
+  repository_id = var.repository_id
+  format        = var.repository_format
 }
