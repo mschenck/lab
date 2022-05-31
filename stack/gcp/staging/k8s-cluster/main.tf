a4357bb0791d2b56419e4dd808edb279e222855b
Michael Schenck <mschenck@digitalocean.com>
2022-05-30 20:52:25 -0400

    more cleanup

--- stack/gcp/staging/k8s-cluster/main.tf
+++ stack/gcp/staging/k8s-cluster/main.tf
@@ -39,11 +39,3 @@ module "k8s-cluster" {
   pods_subnet_name = local.pods_subnet_name
   svcs_subnet_name = local.svcs_subnet_name
 }
-
-# Now enables:
-#
-#    provider "kubernetes" {
-#      host                   = "https://${module.k8s-cluster.endpoint}"
-#      token                  = data.google_client_config.default.access_token
-#      cluster_ca_certificate = base64decode(module.k8s-cluster.master_auth.0.ca_certificate)
-#    }
