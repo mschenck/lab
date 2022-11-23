8dda12487512fedda44e2ee8c82098b913b98a17
Michael Schenck <mschenck@gmail.com>
2022-11-23 11:06:42 -0500

    Also working with nftables

--- src/SCRATCH/build_haproxy.sh
+++ src/SCRATCH/build_haproxy.sh
@@ -63,8 +63,63 @@ backend tcpserver
 EOF
 sudo /usr/local/sbin/haproxy -f /etc/haproxy/haproxy.cfg
 
+ADDR=$(ip address show dev eth1 | egrep -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2)
+
 # iptables
 sudo iptables -t mangle -F
 sudo iptables -t mangle -I PREROUTING \
-     -d 10.0.19.197/32 -p tcp \
+     -d $ADDR/32 -p tcp \
      -j TPROXY --on-port=7331 --on-ip=127.0.0.1
+
+# nftables
+# sudo yum install -y nftables
+
+sudo yum install -y gmp-devel libedit-devel iptables-devel jansson-devel libtool flex bison libreadline
+
+cd /tmp
+wget https://www.netfilter.org/pub/libmnl/libmnl-1.0.5.tar.bz2
+tar -jxf libmnl-1.0.5.tar.bz2
+cd libmnl-1.0.5/
+./configure
+make
+make check
+make install
+cd ..
+
+wget https://www.netfilter.org/pub/libnftnl/libnftnl-1.2.4.tar.bz2
+tar -jxf libnftnl-1.2.4.tar.bz2
+cd libnftnl-1.2.4/
+LIBMNL_LIBS="-L/usr/local/lib -lmnl" LIBMNL_CFLAGS="-I/usr/local/include" ./configure
+make
+make check
+make install
+cd ..
+
+wget https://www.netfilter.org/projects/nftables/files/nftables-1.0.5.tar.bz2
+tar -jxf nftables-1.0.5.tar.bz2
+cd nftables-1.0.5/
+LIBMNL_LIBS="-L/usr/local/lib -lmnl" LIBMNL_CFLAGS="-I/usr/local/include" LIBNFTNL_CFLAGS="-I/usr/local/include" LIBNFTNL_LIBS="-L/usr/local/lib -lnftnl" ./configure --with-xtables --with-json  --disable-man-doc
+make
+make check
+make install
+
+
+cat >/etc/tproxy.nft <<EOF
+table inet edgeports {
+        chain proxy {
+                type filter hook input priority filter; policy accept;
+                iifname "eth1"
+                meta l4proto tcp tproxy ip to 127.0.0.1:7331
+                meta l4proto tcp tproxy ip6 to [::0.1.115.49]
+        }
+}
+EOF
+
+# See [nft (8)](https://www.netfilter.org/projects/nftables/manpage.html)
+nft add table inet edgeports
+
+nft add chain inet edgeports proxy { type filter hook input priority 0 \; policy accept \; }
+
+nft add rule inet edgeports proxy meta iifname eth1
+nft add rule inet edgeports proxy meta l4proto tcp ip tproxy to 127.0.0.1:7331
+nft add rule inet edgeports proxy meta l4proto tcp ip6 tproxy to ::1:7331
