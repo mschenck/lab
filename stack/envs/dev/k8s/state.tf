9c542580ae81cab173f6e9e9ad5959bb3413553e
Michael Schenck <mschenck@gmail.com>
2023-02-17 14:37:31 +0000

    better key name

--- stack/envs/dev/k8s/state.tf
+++ stack/envs/dev/k8s/state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "proxmox-stack-tfstate"
-    key    = "terraform-containers"
+    key    = "terraform-k8s"
     region = "us-east-1"
 
     dynamodb_table = "proxmox-stack-tflocking"
