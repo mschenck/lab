34a6fd0ce1ec644f44d98a555b8964c948c40365
Michael Schenck <mschenck@gmail.com>
2023-02-17 14:37:09 +0000

    fixed missing project attribute

--- stack/modules/gcp/artifact-registry/main.tf
+++ stack/modules/gcp/artifact-registry/main.tf
@@ -4,4 +4,5 @@ resource "google_artifact_registry_repository" "artifact_registry" {
   location      = var.region
   repository_id = var.repository_id
   format        = var.repository_format
+  project       = var.project_id
 }
