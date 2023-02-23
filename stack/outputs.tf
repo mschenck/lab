c9d237614dc7a064860920a80f9940a6e4a39cf6
Michael Schenck <mschenck@gmail.com>
2023-02-23 15:41:05 -0500

    Added missing direction output

--- stack/outputs.tf
+++ stack/outputs.tf
@@ -1,3 +1,8 @@
+output "TerraformState_Directions" {
+  value = module.TerraformState.Directions
+}
+
+
 output "TerraformState_config" {
   value = flatten(module.TerraformState.state_config)[0]
 }
