315a0cc7f09f907eb4ba7159831c66cc2a37cfcf
Michael Schenck <mschenck@gmail.com>
2023-03-31 14:15:19 -0400

    Setup grafana on both clusters and enabled ingress

---
+++ stack/1-Orchestration/charts/grafana/main.tf
@@ -0,0 +1,29 @@
+# Load k8s credentials
+
+provider "helm" {
+  kubernetes {
+    config_path = "/tmp/lab-stack-orchestration-cluster.kubeconfig"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "/tmp/lab-stack-orchestration-cluster.kubeconfig"
+}
+
+# Install grafana Helm chart
+
+module "grafana" {
+  source = "../../../modules/helm"
+
+  helm_chart_name    = var.chart_name
+  helm_repository    = var.repository
+  helm_chart_version = var.chart_version
+
+  install_name      = "grafana"
+  install_namespace = "grafana"
+  install_labels    = var.install_labels
+
+  values_files = [
+    "${file("values.yaml")}"
+  ]
+}
